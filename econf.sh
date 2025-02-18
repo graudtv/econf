0a43fa71f621e68b363a292c10bc44834cc337eb
graudtv <55806873+graudtv@users.noreply.github.com>
2025-02-18 20:18:50 +0300

    new commands and fixes

--- econf.sh
+++ econf.sh
@@ -24,7 +24,7 @@ __econf_help() {
   echo "  econf --mv <src-name> <dst-name>        # rename config"
   echo "  econf --cp <src-name> <dst-name>        # copy config"
   echo "  econf --cpe <src-name> <dst-name>       # copy and edit config"
-  echo "  econf --rm|--remove <config-name>       # remove config"
+  echo "  econf --rm <config-name>                # remove config"
   echo "  econf --realpath <config-name>          # get path to config file"
   echo
   echo "Helper commands:"
@@ -37,35 +37,87 @@ __econf_help() {
   echo "                                          # prepend path to \$PATH"
   echo "  econf -pl|--prepend-lib-dir <directory> # prepend library to \$LD_LIBRARY_PATH"
   echo "  econf --append-bin-dir <directory>      # append path to \$PATH"
+  echo "  econf --require-file <path>             # check that file exists"
+  echo "  econf --require-dir <path>              # check that directory exists"
+  echo "  econf --getname                         # get name of currently processed config"
   echo
   echo "User configs directory: $ECONF_CONFIG_DIR"
 }
 
+# Return the name of config which is currently being executed or empty string
+__econf_current_config() {
+  echo "$__ECONF_CONFIG_STACK" | awk -F ':' -e '{if (NF > 0) print $(NF)}'
+}
+
+__econf_source_config() {
+  __ECONF_CONFIG_STACK="${__ECONF_CONFIG_STACK}:${1}"
+  source "$ECONF_CONFIG_DIR/$1"
+  __ECONF_CONFIG_STACK=${__ECONF_CONFIG_STACK%:${1}}
+}
+
+__econf_log() {
+  if [ -n "$(__econf_current_config)" ]; then
+    echo "econf: $(__econf_current_config): $@" >&2
+  else
+    echo "econf: $@" >&2
+  fi
+}
+
+__econf_hint() {
+  if [ -z "$(__econf_current_config)" ]; then
+    __econf_log "$@"
+  fi
+}
+
+__econf_sanity_check() {
+  # Prevent sanity-check fork bomb
+  if [ "$__ECONF_SANITY_CHECK" = "1" ]; then
+    return
+  fi
+
+  # TODO: return 1 if at least one error found
+  export __ECONF_SANITY_CHECK=1
+  {
+    local cfg
+    for cfg in $(ls $ECONF_CONFIG_DIR); do
+      bash -lic "econf '$cfg' 2>&1" | head -1
+    done
+  } | perl -ne 'print unless $used{$_}; $used{$_} = 1';
+  unset __ECONF_SANITY_CHECK
+}
+
 __econf_expect_zero_args() {
   if [ "$#" -gt 1 ]; then
-    echo "econf $1: too many arguments specified, zero expected"
+    __econf_log "too many arguments for '$1' command, zero expected"
     return 1
   fi
 }
 
 __econf_expect_one_arg() {
   if [ "$#" -gt 2 ]; then
-    echo "econf $1: too many arguments specified, only one expected"
+    __econf_log "too many arguments for '$1' command, 1 expected"
     return 1
   fi
   if [ "$#" -lt 2 ]; then
-    echo "econf $1: expected 1 argument, got 0"
+    __econf_log "expected 1 argument for '$1' command, got 0"
     return 1
   fi
 }
 
 __econf_expect_two_args() {
   if [ "$#" -gt 3 ]; then
-    echo "econf $1: too many arguments specified, two expected"
+    __econf_log "too many arguments for '$1' command, 2 expected"
     return 1
   fi
   if [ "$#" -lt 3 ]; then
-    echo "econf $1: expected 2 arguments, got $(expr $# - 1)"
+    __econf_log "expected 2 arguments for '$1' command, got $(expr $# - 1)"
+    return 1
+  fi
+}
+
+__econf_assert_not_in_config() {
+  if [ -n "$(__econf_current_config)" ]; then
+    __econf_log "'$1' should not be run within config file"
     return 1
   fi
 }
@@ -82,10 +134,6 @@ __econf_loadenv() {
 }
 
 __econf_list_configs() {
-  # if [ "$1" == "-l" ]; then
-  #   ls "$ECONF_CONFIG_DIR"
-  #   return
-  # fi
   local cfg header configs
   if [ "$#" = 1 ]; then
     configs=$(ls $ECONF_CONFIG_DIR)
@@ -110,27 +158,29 @@ econf() {
     __econf_help
   elif [ "$1" = "-l" -o "$1" = "--ls" -o "$1" = "--list" ]; then
     echo "Available configs:"
-    __econf_list_configs | sed 's/^/  /'
-  elif [ "$1" = "-lp" -o "$1" = "--lp" ]; then
-    econf -l profile
+    __econf_list_configs "$@" | sed 's/^/  /'
   elif [ "$1" = "-e" -o "$1" = "--edit" ]; then
     __econf_expect_one_arg "$@" || return
     mkdir -p $(dirname -- "$ECONF_CONFIG_DIR/$2")
     $EDITOR "$ECONF_CONFIG_DIR/$2"
   elif [ "$1" = "--mv" ]; then
     __econf_expect_two_args "$@" || return
+    __econf_assert_not_in_config "$@" || return
     mv "$ECONF_CONFIG_DIR/$2" "$ECONF_CONFIG_DIR/$3"
   elif [ "$1" = "--cp" ]; then
     __econf_expect_two_args "$@" || return
+    __econf_assert_not_in_config "$@" || return
     cp "$ECONF_CONFIG_DIR/$2" "$ECONF_CONFIG_DIR/$3"
   elif [ "$1" = "--cpe" ]; then
     __econf_expect_two_args "$@" || return
+    __econf_assert_not_in_config "$@" || return
     cp "$ECONF_CONFIG_DIR/$2" "$ECONF_CONFIG_DIR/$3"
     $EDITOR "$ECONF_CONFIG_DIR/$3"
-  elif [ "$1" = "--rm" -o "$1" = "--remove" ]; then
+  elif [ "$1" = "--rm" ]; then
     __econf_expect_one_arg "$@" || return
+    __econf_assert_not_in_config "$@" || return
     if ! __econf_config_exists "$2"; then
-      echo "econf $1: no such config '$2'"
+      __econf_log "$1: no such config '$2'"
       return 1
     fi
     rm "$ECONF_CONFIG_DIR/$2"
@@ -150,19 +200,19 @@ econf() {
   elif [ "$1" = "-pb" -o "$1" = "--pp" -o "$1" = "--ppath" -o $1 = "--prepend-bin-dir" ]; then
     __econf_expect_one_arg "$@" || return
     if ! [ -e "$2" ]; then
-      echo "econf $1: error: directory '$2' does not exists"; return 1
+      __econf_log "$1: directory '$2' does not exist"; return 1
     fi
     if ! [ -d "$2" ]; then
-      echo "econf $1: error: '$2' is not a directory"; return 1
+      __econf_log "$1: '$2' is not a directory"; return 1
     fi
     export PATH="$(realpath $2):$PATH"
   elif [ "$1" = "-pl" -o "$1" = "--prepend-lib-dir" ]; then
     __econf_expect_one_arg "$@" || return
     if ! [ -e "$2" ]; then
-      echo "econf $1: error: directory '$2' does not exists"; return 1
+      __econf_log "$1: directory '$2' does not exist"; return 1
     fi
     if ! [ -d "$2" ]; then
-      echo "econf $1: error: '$2' is not a directory"; return 1
+      __econf_log "$1: '$2' is not a directory"; return 1
     fi
     export LD_LIBRARY_PATH="$(realpath $2):$LD_LIBRARY_PATH"
   elif [ "$1" = "--append-bin-dir" ]; then
@@ -174,16 +224,27 @@ econf() {
   elif [ "$1" = "--loadenv" ]; then
     __econf_expect_one_arg "$@" || return
     __econf_loadenv "$@"
+  elif [ "$1" = "--require-file" -o "$1" = "--require-dir" ]; then
+    __econf_expect_one_arg "$@" || return
+    if ! [ -e "$2" ]; then
+      __econf_log "missing required file or directory '$2'"
+    fi
+  elif [ "$1" = "--sanity" ]; then
+    __econf_assert_not_in_config "$@" || return
+    __econf_sanity_check
+  elif [ "$1" = "--getname" ]; then
+    __econf_current_config
   elif [ "${1:0:1}" = "-" ]; then
-    echo "econf: unknown command '$1'"
-    echo "econf: try 'econf --help'"
+    __econf_log "econf: unknown command '$1'"
+    __econf_hint "econf: try 'econf --help'"
     return 1
   elif ! [ -f "$ECONF_CONFIG_DIR/$1" ]; then
-    echo "econf: no such config '$1'"
-    echo
-    echo "Available configs:"
-    __econf_list_configs | sed 's/^/  /'
+    __econf_log "econf: no such config '$1'"
+    echo >&2
+    echo "Available configs:" >&2
+    __econf_list_configs | sed 's/^/  /' >&2
+    return 1
   else
-    source $ECONF_CONFIG_DIR/$1
+    __econf_source_config "$1"
   fi
 }
